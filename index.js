eec091c1612dcb4706afc304cb44ce9793bfc49e
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-03-31 12:10:08 +0500

    add chalk functionality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 //SHEBANG
+import chalk from "chalk";
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter Second number", type: "number", name: "SecondtNumber" },
-    { message: "Select one of the operator to perfom actions",
+    { message: chalk.blue("Enter first your number"), type: "number", name: "firstNumber" },
+    { message: chalk.blue("Enter Second your number"), type: "number", name: "SecondtNumber" },
+    { message: chalk.yellow("Select one of the operator to perfom actions"),
         type: "list",
         name: "oprator",
         choices: ["Adition", "Subtraction", "Multiplication", "Division"],
@@ -12,18 +13,19 @@ const answer = await inquirer.prompt([
 ]);
 //conditional statements
 if (answer.oprator == "Adition") {
-    console.log("your answer is :" + (answer.firstNumber + answer.SecondtNumber));
+    console.log(chalk.green.bold("your answer is :" + (answer.firstNumber + answer.SecondtNumber)));
 }
 else if (answer.oprator == "Subtraction") {
-    console.log("your answer is :" + (answer.firstNumber - answer.SecondtNumber));
+    console.log(chalk.blueBright.bold("your answer is :" + (answer.firstNumber - answer.SecondtNumber)));
 }
 else if (answer.oprator == "Multiplication") {
-    console.log("your answer is :" + (answer.firstNumber * answer.SecondtNumber));
+    console.log(chalk.magenta.bold("your answer is :" + (answer.firstNumber * answer.SecondtNumber)));
 }
 else if (answer.oprator == "Division") {
-    console.log("your answer is :" + (answer.firstNumber / answer.SecondtNumber));
+    console.log(chalk.cyanBright.bold("your answer is :" + (answer.firstNumber / answer.SecondtNumber)));
 }
 else {
-    console.log("please select a valid oprator ");
+    console.log(chalk.redBright.bold("please select a valid oprator "));
 }
 ;
+console.log(chalk.greenBright.italic(`         THE END        `));
